d1f285344cdf1bc272286d82a791daeb57c7d8c4
Nalinikanth <nalini.kanth99@gmail.com>
2016-06-30 22:04:20 +0530

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -1,13 +1,20 @@
+/**
+ * Created by nalinim on 6/30/16.
+ */
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+
 public class TestRunner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(FacebookLoginTest.class);
+    public static void main(String[] args) throws FailureException {
+        Result result = JUnitCore.runClasses(FacebookLoginTest.class);
+        if(result.getFailureCount() != 0){
+            throw new FailureException();
+        }
       for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
+          System.out.println(failure.toString());
       }
       System.out.println(result.wasSuccessful());
-   }
-}  
+    }
+}
