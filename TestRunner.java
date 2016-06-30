a8eb40736039bdd863af09746947fe0e819ee1c5
Nalinikanth <nalini.kanth99@gmail.com>
2016-06-30 22:43:04 +0530

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -9,12 +9,18 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
     public static void main(String[] args) throws FailureException {
         Result result = JUnitCore.runClasses(FacebookLoginTest.class);
-        if(result.getFailureCount() != 0){
-            throw new FailureException();
+        for (Failure failure : result.getFailures()) {
+            //System.out.println(failure.toString());
+            System.out.println(failure);
         }
-      for (Failure failure : result.getFailures()) {
-          System.out.println(failure.toString());
-      }
-      System.out.println(result.wasSuccessful());
+
+
+        System.out.println("Number of Tests ran "+ result.getRunCount() );
+        if(result.getFailureCount() != 0){
+            throw new FailureException(result.getFailureCount());
+        }
+
+        System.out.println("Number of Tests Passed "+ result.getRunCount());
+
     }
 }
